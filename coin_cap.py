208f85869a8047719357fab1a9bcc57f46382944
slavkoleminh <slavko007997@gmail.com>
2023-12-18 13:36:07 +0100

    working simulation

--- coin_cap.py
+++ coin_cap.py
@@ -2,13 +2,26 @@ import requests
 import pandas as pd
 import talib
 import matplotlib.pyplot as plt
+from datetime import datetime
+
+def days_until_current_date(target_date):
+    # Convert the input date to a datetime object
+    target_datetime = datetime.strptime(target_date, "%Y-%m-%d")
+
+    # Get the current date
+    current_datetime = datetime.now()
+
+    # Calculate the difference in days
+    days_difference = (current_datetime - target_datetime).days
+
+    return days_difference
 
 # Function to get historical Bitcoin prices from CoinGecko API
-def get_historical_prices():
+def get_historical_prices(days):
     url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
     params = {
         'vs_currency': 'eur',
-        'days': '3500',
+        'days': days,
         'interval': 'daily',  # Change to daily interval
     }
 
@@ -38,6 +51,7 @@ def plot_data(prices, timeframe):
     ax1.set_xlabel('Date')
     ax1.set_ylabel('Bitcoin Price (EUR)', color='tab:blue')
     ax1.plot(prices['time'], prices['price'], color='tab:blue')
+    ax1.set_yscale('log')  # Set y-axis scale to logarithmic
     ax1.tick_params(axis='y', labelcolor='tab:blue')
 
     ax2 = ax1.twinx()
@@ -48,9 +62,47 @@ def plot_data(prices, timeframe):
     plt.title(f'Bitcoin Price and RSI Over the Last Year ({timeframe})')
     plt.show()
 
+def simulate_trading(prices, from_date=None, until_date=None):
+    # Filter prices based on the specified date range
+    if from_date:
+        prices = prices[prices['time'] >= pd.to_datetime(from_date)]
+    if until_date:
+        prices = prices[prices['time'] <= pd.to_datetime(until_date)]
+
+    capital = 10000  # Starting capital in EUR
+    holding = 0  # Initial holding in BTC
+    # rsi_buy_threshold = 45
+    # rsi_sell_threshold = 85
+    rsi_buy_threshold = 50
+    rsi_sell_threshold = 65
+
+    for index, row in prices.iterrows():
+        if row['rsi'] <= rsi_buy_threshold and capital > 0:
+            # Buy BTC with available capital
+            btc_bought = capital / row['price']
+            holding += btc_bought
+            capital = 0
+            print(f"Buy BTC at {row['time']} - RSI: {row['rsi']}, Price: {row['price']}, Holding: {holding}")
+
+        elif row['rsi'] >= rsi_sell_threshold and holding > 0:
+            # Sell all BTC
+            capital += holding * row['price']
+            holding = 0
+            print(f"Sell BTC at {row['time']} - RSI: {row['rsi']}, Price: {row['price']}, Capital: {capital}")
+
+    # If still holding at the end, sell
+    if holding > 0:
+        capital += holding * prices.iloc[-1]['price']
+        print(f"Sell remaining BTC at {prices.iloc[-1]['time']} - Price: {prices.iloc[-1]['price']}, Capital: {capital}")
+
+    return capital
+
 def main():
+    target_date = "2015-01-01"
+    days = days_until_current_date(target_date)
+
     # Get historical Bitcoin prices
-    bitcoin_prices = get_historical_prices()
+    bitcoin_prices = get_historical_prices(days)
 
     # Convert to weekly prices
     timeframe = 'M'  # You can change this to 'M' for monthly, 'D' for daily, etc.
@@ -59,6 +111,13 @@ def main():
     # Calculate RSI
     bitcoin_prices_timeframe = calculate_rsi(bitcoin_prices_timeframe)
 
+    from_date = '2018-01-01'
+    until_date = '2022-12-31'
+
+    # Simulate trading
+    final_capital = simulate_trading(bitcoin_prices_timeframe, from_date, until_date)
+    print(f"Final capital after simulation: {final_capital} EUR")
+
     # Plot data
     plot_data(bitcoin_prices_timeframe, timeframe)
 
@@ -66,3 +125,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
